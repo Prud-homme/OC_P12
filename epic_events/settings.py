14e4517631bbe4ace9200876cc96e59d73047d3a
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-09-02 15:16:56 +0200

    Update django config for new apps

--- epic_events/settings.py
+++ epic_events/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'crm',
+    'crm.client',
+    'crm.contract',
+    'crm.event',
     'authentication',
 ]
 
