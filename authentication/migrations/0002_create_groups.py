3c9f23e555dfdb6f6cde2bbf7cc644dc7e48ecf2
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-09-09 19:33:08 +0200

    Update to start the migration from a new install

--- authentication/migrations/0002_create_groups.py
+++ authentication/migrations/0002_create_groups.py
@@ -2,29 +2,72 @@
 
 from django.db import migrations
 
+
 def create_groups(apps, schema_migration):
-    User = apps.get_model('authentication', 'User')
-    Group = apps.get_model('auth', 'Group')
-    Permission = apps.get_model('auth', 'Permission')
+    Group = apps.get_model("auth", "Group")
+    Permission = apps.get_model("auth", "Permission")
+    ContentType = apps.get_model("contenttypes", "ContentType")
 
-    add_user = Permission.objects.get(codename='add_user')
-    change_user = Permission.objects.get(codename='change_user')
-    delete_user = Permission.objects.get(codename='delete_user')
-    view_user = Permission.objects.get(codename='view_user')
+    User = apps.get_model("authentication", "User")
+    Client = apps.get_model("client", "Client")
+    Contract = apps.get_model("contract", "Contract")
+    Event = apps.get_model("event", "Event")
 
-    view_group = Permission.objects.get(codename='view_group')
+    sales_group, created = Group.objects.get_or_create(name="sales")
+    support_group, created = Group.objects.get_or_create(name="support")
+    management_group, created = Group.objects.get_or_create(name="management")
 
-    add_client = Permission.objects.get(codename='add_client')
-    change_client = Permission.objects.get(codename='change_client')
-    view_client = Permission.objects.get(codename='view_client')
-    
-    add_contract = Permission.objects.get(codename='add_contract')
-    change_contract = Permission.objects.get(codename='change_contract')
-    view_contract = Permission.objects.get(codename='view_contract')
+    ct = ContentType.objects.get_for_model(User)
+    add_user = Permission.objects.create(
+        codename="add_user", name="Can add user", content_type=ct
+    )
+    change_user = Permission.objects.create(
+        codename="change_user", name="Can change user", content_type=ct
+    )
+    delete_user = Permission.objects.create(
+        codename="delete_user", name="Can delete user", content_type=ct
+    )
+    view_user = Permission.objects.create(
+        codename="view_user", name="Can view user", content_type=ct
+    )
 
-    add_event = Permission.objects.get(codename='add_event')
-    change_event = Permission.objects.get(codename='change_event')
-    view_event = Permission.objects.get(codename='view_event')
+    ct = ContentType.objects.get_for_model(Group)
+    view_group = Permission.objects.create(
+        codename="view_group", name="Can view group", content_type=ct
+    )
+
+    ct = ContentType.objects.get_for_model(Client)
+    add_client = Permission.objects.create(
+        codename="can_add_client", name="Can add client", content_type=ct
+    )
+    change_client = Permission.objects.create(
+        codename="change_client", name="Can change client", content_type=ct
+    )
+    view_client = Permission.objects.create(
+        codename="view_client", name="Can view client", content_type=ct
+    )
+
+    ct = ContentType.objects.get_for_model(Contract)
+    add_contract = Permission.objects.create(
+        codename="add_contract", name="Can add contract", content_type=ct
+    )
+    change_contract = Permission.objects.create(
+        codename="change_contract", name="Can change contract", content_type=ct
+    )
+    view_contract = Permission.objects.create(
+        codename="view_contract", name="Can view contract", content_type=ct
+    )
+
+    ct = ContentType.objects.get_for_model(Event)
+    add_event = Permission.objects.create(
+        codename="add_event", name="Can add event", content_type=ct
+    )
+    change_event = Permission.objects.create(
+        codename="change_event", name="Can change event", content_type=ct
+    )
+    view_event = Permission.objects.create(
+        codename="view_event", name="Can view event", content_type=ct
+    )
 
     sales_permissions = [
         add_client,
@@ -33,7 +76,7 @@ def create_groups(apps, schema_migration):
         change_contract,
         view_contract,
         add_event,
-     ]
+    ]
 
     support_permissions = [
         change_event,
@@ -54,33 +97,19 @@ def create_groups(apps, schema_migration):
         view_event,
         view_group,
     ]
-    
-    sales_team = Group(name='sales_permissions')
-    sales_team.save()
-    sales_team.permissions.set(sales_permissions)
 
-    support_team = Group(name='support_team')
-    support_team.save()
-    support_team.permissions.set(support_permissions)
+    sales_group.permissions.set(sales_permissions)
+    support_group.permissions.set(support_permissions)
+    management_group.permissions.set(management_permissions)
 
-    management_team = Group(name='management_team')
-    management_team.save()
-    management_team.permissions.set(management_permissions)
-    
-    for user in User.objects.all():
-        if user.role == 'SALES':
-            sales_team.user_set.add(user)
-        if user.role == 'SUPPORT':
-            support_team.user_set.add(user)
-        if user.role == 'MANAGEMENT':
-            management_team.user_set.add(user)
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ("authentication", "0001_initial"),
+        ("client", "0001_initial"),
+        ("contract", "0001_initial"),
+        ("event", "0001_initial"),
     ]
 
-    operations = [
-        migrations.RunPython(create_groups)
-    ]
+    operations = [migrations.RunPython(create_groups)]
