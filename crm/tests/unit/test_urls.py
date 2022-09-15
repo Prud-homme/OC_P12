8bfe72ad156eae568cf7efa94c2239eec44334d7
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-09-15 16:09:13 +0200

    Token obtain/refresh URL testing

---
+++ crm/tests/unit/test_urls.py
@@ -0,0 +1,16 @@
+import pytest
+
+from django.urls import reverse, resolve
+
+
+@pytest.mark.django_db
+def test__token_urls():
+    path = reverse('token_obtain_pair', kwargs={})
+    
+    assert path == "/api/v1/token/"
+    assert resolve(path).view_name == "token_obtain_pair"
+
+    path = reverse('token_refresh', kwargs={})
+    
+    assert path == "/api/v1/token/refresh/"
+    assert resolve(path).view_name == "token_refresh"
